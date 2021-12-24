be44ce8fd533bf670bb50fd1db4b6efa6af906b2
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-12-24 21:44:15 +0700

    refactor style theme2 call to action

---
+++ resources/views/components/theme2/button-outline.blade.php
@@ -0,0 +1,3 @@
+<button class="py-4 px-6 border border-primary rounded-md bg-white text-primary shadow-lg">
+    Apply Now
+</button>
--- resources/views/theme2/call-to-action.blade.php
+++ resources/views/theme2/call-to-action.blade.php
@@ -1,10 +1,13 @@
 <section {{ $attributes }}>
-    <h2 class="text-xl text-primary font-semibold text-center uppercase">And Many More</h2>
-    <p class="text-center text-gray-600 mt-2">More than 10,000+ people have registered.</p>
-    <div class="text-center mt-2">
-        <x-theme2.button-outline class="shadow-lg" />
-        <button class="mt-6 py-4 px-6 border border-primary rounded-md bg-white text-primary shadow-lg">
-            Apply Now
-        </button>
-    </div>
+    <x-theme2.container class="lg:flex items-center justify-center">
+        <div>
+            <h2 class="text-xl text-primary font-semibold text-center uppercase">And Many More</h2>
+            <p class="text-center text-gray-600 mt-2">More than 10,000+ people have registered.</p>
+        </div>
+        <div class="text-center mt-8 lg:mt-0 lg:ml-10">
+            <x-theme2.button-outline>
+                Apply Now
+            </x-theme2.button-outline>
+        </div>
+    </x-theme2.container>
 </section>
